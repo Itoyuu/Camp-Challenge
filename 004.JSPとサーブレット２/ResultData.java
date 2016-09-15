fa47de9343c9a57ab59dc7c9f9a4e764291786e7
geekjob-support <geekjob-support@groove-gear.jp>
2016-09-15 18:28:11 +0900

    提出修正

---
+++ 004.JSPとサーブレット２/ResultData.java
@@ -0,0 +1,41 @@
+package org.camp.servlet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ResultData implements Serializable{
+    private Date d;
+    private String luck;
+    
+public  ResultData (){
+}
+
+
+/**
+ *
+ * @author itou
+ */
+public Date getD (){
+ return d;
+}  
+
+
+public void setD(Date d) {
+    this.d = d;
+}
+public String getLuck() {
+   return luck;
+}
+
+public void setLuck(String luck){
+    this.luck = luck;
+}
+
+}
